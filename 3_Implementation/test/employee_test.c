26911f3ae5d3f9eb14fc266549b9b1947c0844c2
Vakash123 <vankadari.akash.nanda.2000@gmail.com>
2022-02-12 17:59:51 +0530

    update

--- 3_Implementation/test/employee_test.c
+++ 3_Implementation/test/employee_test.c
@@ -14,33 +14,32 @@
 #include <main.h>
 #define PROJECT_NAME    "Employee_Record_System"
 
-/* Prototypes for all the test functions */
+
 void position(void);
 
 
-/* Required by the unity test framework */
 void setUp(){}
-/* Required by the unity test framework */
+
 void tearDown(){}
 
-/* Start of the application test */
+
 int main()
 {
-/* Initiate the Unity Test Framework */
+
   UNITY_BEGIN();
 
-/* Run Test functions */
+
   RUN_TEST(position);
   
 
-  /* Close the Unity Test Framework */
+
   return UNITY_END();
 }
 
-/* Write all the test functions */ 
+
 void position(void) {
   TEST_ASSERT_EQUAL(20 40, position(20,40));
   
-  /* Dummy fail*/
+ 
   TEST_ASSERT_EQUAL(20 42, position(20,40));
 }
